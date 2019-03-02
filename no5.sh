90b6ea53b4705be8150f4ba4b58ca500bb681502
mohadam83 <moh.adam.83@gmail.com>
2019-03-02 11:49:35 +0700

    baru

--- no5.sh
+++ no5.sh
@@ -1,1 +1,1 @@
-awk '(/cron/ || /CRON/) && (!/sudo/) && (NF < 13) {print}' /var/log/syslog >> /home/salsha/sisop/no5.log
+awk '(tolower($0) ~ /cron/) && (tolower($0) !~ /sudo/) && (NF < 13) {print}' /var/log/syslog >> /home/adam/SoalShift_modul1_C8/no5.log
